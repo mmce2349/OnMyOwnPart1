bd4212c4c1891570a4afe094e517a4543f6e8d65
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-09-07 08:42:33 -0600

    Added new lines of text

--- src/alone/controller/AloneController.java
+++ src/alone/controller/AloneController.java
@@ -4,5 +4,7 @@ public class AloneController {
 	public void start()
 	{
 		System.out.println("Hi, my name is Mitch.");
+		System.out.println("I don't know what else to say.");
+		System.out.println("This is great.");
 	}
 }
